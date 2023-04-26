02a466677b824370f5b2efde8d88572007b4e8a7
DAMISSINGKODA <131887002+DAMISSINGKODA@users.noreply.github.com>
2023-04-26 15:10:18 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,27 @@
-basic.showLeds(`
-    . # . . .
-    . # . # .
-    . # # . .
-    . # # . .
-    . # . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . # . # .
-    # . . . #
-    `)
-basic.showLeds(`
-    . . # . .
-    . . . . .
-    . . # . .
-    . . # . .
-    . . # . .
-    `)
-basic.forever(function () {
-    music.playMelody("B A G A G F A C5 ", 73)
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . . .
+        . # . # .
+        . # # . .
+        . # # . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Ghost)
+})
+music.playMelody("B A G A G F A C5 ", 73)
